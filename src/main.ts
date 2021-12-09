b9fc244e4510d11e5a91f135e6e2b7cc934da024
Nelson <nelson.s.neves@gmail.com>
2021-12-09 16:37:38 +0000

    Upgrade dependencies to use nestjs 8

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { AppModule } from "./AppModule";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from "./configs/ValidationPipe";
+import { Environment } from './services/Environment';
+import { ConsoleLogger } from './services/Logger/ConsoleLogger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    const environmentInstance = app.get("Environment");
-    const loggerInstance = app.get("ConsoleLogger");
 
+    const environmentInstance = app.get(Environment);
+    const loggerInstance = app.get(ConsoleLogger);
     const options = new DocumentBuilder()
         .setTitle('Simple-Auth')
-        .setDescription('This document will describe the avilable endpoints this service')
+        .setDescription('This document will describe the available endpoints this service')
         .setVersion('0.1.0')
         .addBearerAuth(
             { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
